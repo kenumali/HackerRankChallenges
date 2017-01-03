baec0e7a897862bfc47651ea7f09a0f1ba9a9d03
kenumali <kenneth.n.umali@gmail.com>
2017-01-03 12:21:24 +0000

    Create OutputFormatting.java

---
+++ java/introduction/OutputFormatting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/*
+Link to problem: https://www.hackerrank.com/challenges/java-output-formatting
+*/
+
+public class OutputFormatting {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("================================");
+    for(int i=0;i<3;i++){
+      String s1=sc.next();
+      int x=sc.nextInt();
+      System.out.printf("%1$-14s %2$03d\n", s1, x);
+    }
+    System.out.println("================================");
+  }
+}
