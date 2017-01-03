8bea8e50008f219ef4156d578d02005493be8051
kenumali <kenneth.n.umali@gmail.com>
2017-01-03 12:34:37 +0000

    Create EndOfFile.java

---
+++ java/introduction/EndOfFile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/*
+Link to problem: https://www.hackerrank.com/challenges/java-end-of-file
+*/
+
+public class EndOfFile {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+		int i = 1;
+		while(sc.hasNext()) {
+			System.out.println(i + " " + sc.nextLine());
+      i++;
+		}
+  }
+}
