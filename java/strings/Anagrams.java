b6dd27ccd5dc9425c8df173a6334405763c03744
kenumali <kenneth.n.umali@gmail.com>
2017-01-03 14:08:28 +0000

    Create Anagrams.java

---
+++ java/strings/Anagrams.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Link to problem: https://www.hackerrank.com/challenges/java-anagrams
+*/
+
+public class Anagrams {
+	static boolean isAnagram(String a, String b) {
+		char[] arrayA = a.toLowerCase().toCharArray();
+		char[] arrayB = b.toLowerCase().toCharArray();
+
+		Arrays.sort(arrayA);
+		Arrays.sort(arrayB);
+		return Arrays.equals(arrayA, arrayB) ? true : false;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		boolean ret = isAnagram(a, b);
+		System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+	}
+}
