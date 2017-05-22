0cf89f0aa1b1b6a8fa1890d13e092bb52eadd6af
Kenneth Umali <kenneth.n.umali@gmail.com>
2017-05-22 11:49:04 +0100

    Create DiwaliLights.java

---
+++ mathematics/fundamentals/DiwaliLights.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class DiwaliLights {
+	final static BigInteger BULB_STATUS = BigInteger.valueOf(2);
+	final static BigInteger OFF = BigInteger.ONE;
+	final static BigInteger MODULUS = BigInteger.valueOf(100000);
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCases = scanner.nextInt();
+		for(int i = 0; i < testCases; i++) {
+			System.out.println(BULB_STATUS.pow(scanner.nextInt()).subtract(OFF).mod(MODULUS));
+		}
+	}
+}
